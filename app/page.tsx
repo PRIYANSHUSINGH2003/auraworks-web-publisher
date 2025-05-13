fe7b5b44c5c97ab01b8bf3e382705fa138958f22
Priyanshu Singh Rajawat <priyanshusingh00004@gmail.com>
2025-05-13 13:28:04 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+// import Image from "next/image";
 import TopbarHeader from "@/components/TopbarHeader";
 import LeftSideBar from "@/components/LeftSideBar";
 import Dashboard from "@/components/Dashboard";
